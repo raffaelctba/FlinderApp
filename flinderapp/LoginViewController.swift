4b5938e81cb1167fda7155edfb9d1a1c047e1603
raffaelctba <raffael@Raffaels-MacBook-Pro.local>
2016-06-19 03:19:10 -0700

    third commit
    Implemented the login functionality.

---
+++ flinderapp/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.swift
+//  flinderapp
+//
+//  Created by Raffael on 2016-06-19.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate{
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        if(FBSDKAccessToken.currentAccessToken()==nil){
+            print("User is not logged in")
+        }else{
+            
+           print("User is logged in")
+        }
+        loginButton.delegate = self
+        loginButton.readPermissions=["public_profile","email", "user_friends"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!){
+        
+        
+        if(error != nil){
+            print(error.localizedDescription)
+            return
+        }
+        
+        if let userToken = result.token
+        {
+            //Get user access token
+            let token:FBSDKAccessToken = result.token
+            print("Token = \(FBSDKAccessToken.currentAccessToken().tokenString)")
+             print("UserId = \(FBSDKAccessToken.currentAccessToken().userID)")
+        
+         let protectedPage = self.storyboard?.instantiateViewControllerWithIdentifier("ProtectedPageViewController") as! ProtectedPageViewController
+            
+            let ProtectedPageNav = UINavigationController.init(rootViewController: protectedPage)
+            
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            
+        appDelegate.window?.rootViewController = ProtectedPageNav
+        }
+    }
+    
+
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!){
+        print("User is logged out")
+    }
+}
