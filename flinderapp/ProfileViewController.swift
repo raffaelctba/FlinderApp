097239f6f73289c0861e8d6d561fb2ecf2a73087
Mathias Garcia <mathiasdpg@gmail.com>
2016-06-28 17:56:53 -0700

    ok
    ok

--- flinderapp/ProfileViewController.swift
+++ flinderapp/ProfileViewController.swift
@@ -6,9 +6,26 @@
 //  Copyright © 2016 Mathias Garcia. All rights reserved.
 //
 
+
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
 
 class ProfileViewController: UIViewController {
+
+    @IBAction func logoutButtonTapped(sender: AnyObject) {
+        let loginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        let loginPage = self.storyboard?.instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
+        
+        let loginPageNav = UINavigationController(rootViewController: loginPage)
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        appDelegate.window!.rootViewController = loginPageNav
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
